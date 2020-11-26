72b64d92d89a9f10d222963bf083eaee86fb5ab7
Shaun <shaun.lum@gov.bc.ca>
2020-11-26 09:46:06 -0800

    css

--- minimal_lite/js/custom/ednet.js
+++ minimal_lite/js/custom/ednet.js
@@ -1,6 +1,9 @@
 (function ($) {
-    $('.search-link a').click(function(event){
-        event.preventDefault();
-        $('.search-block-form').slideToggle();
+    var tallestHeight = "0"
+    $(".highlight-summary").each( function( index, element ){
+        if($(this).height() >= tallestHeight){
+            tallestHeight = $(this).height();
+        }
     });
+    $(".highlight-summary").css("height",tallestHeight);
 }(jQuery));
